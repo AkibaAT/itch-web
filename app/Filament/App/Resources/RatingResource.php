88232a92bc90716c4b848de325230f7a7c84f232
Akiba <admin@akiba.games>
2024-03-12 01:05:06 +0100

    Add stack

--- app/Filament/App/Resources/RatingResource.php
+++ app/Filament/App/Resources/RatingResource.php
@@ -9,6 +9,7 @@ use App\Models\Rating;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Resource;
 use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Filters\SelectFilter;
@@ -24,51 +25,56 @@ class RatingResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
-            ->columns([Split::make([
-                TextColumn::make('published_at')
-                    ->width('1%')
-                    ->dateTime()
-                    ->sortable()
-                    ->description('Published at', position: 'above'),
-                TextColumn::make('game.name')
-                    ->tooltip('Filter by this game')
-                    ->disabledClick()
-                    ->extraAttributes(function (Rating $record) {
-                        return [
-                            'wire:click' => '$set("tableFilters.game.value", "' . $record->game_id . '", true)',
-                            'class' => 'transition hover:text-primary-500 cursor-pointer',
-                        ];
-                    })
-                    ->sortable()
-                    ->description('Game', position: 'above'),
-                TextColumn::make('rating')
-                    ->width('1%')
-                    ->numeric()
-                    ->sortable()
-                    ->description('Rating', position: 'above'),
-                TextColumn::make('review')
-                    ->html()
-                    ->wrap()
-                    ->description('Review', position: 'above'),
-                TextColumn::make('rater_id')
-                    ->label('Rater ID')
-                    ->numeric()
-                    ->tooltip('Filter by this rater')
-                    ->disabledClick()
-                    ->extraAttributes(function (Rating $record) {
-                        return [
-                            'wire:click' => '$set("tableFilters.rater_id.value", "' . $record->rater_id . '", true)',
-                            'class' => 'transition hover:text-primary-500 cursor-pointer',
-                        ];
-                    })
-                    ->description('Rater ID', position: 'above'),
-                TextColumn::make('event_id')
-                    ->label('Rating ID')
-                    ->numeric()
-                    ->url(fn (Rating $record) => 'https://itch.io/event/' . $record->event_id)
-                    ->openUrlInNewTab()
-                    ->description('Rating ID', position: 'above'),
-            ])->from('xl')])
+            ->columns([
+                Split::make([
+                    Stack::make([
+                        TextColumn::make('game.name')
+                            ->tooltip('Filter by this game')
+                            ->disabledClick()
+                            ->extraAttributes(function (Rating $record) {
+                                return [
+                                    'wire:click' => '$set("tableFilters.game.value", "' . $record->game_id . '", true)',
+                                    'class' => 'transition hover:text-primary-500 cursor-pointer',
+                                ];
+                            })
+                            ->sortable()
+                            ->description('Game', position: 'above'),
+                        TextColumn::make('rating')
+                            ->width('1%')
+                            ->numeric()
+                            ->sortable()
+                            ->description('Rating', position: 'above'),
+                        TextColumn::make('published_at')
+                            ->width('1%')
+                            ->dateTime()
+                            ->sortable()
+                            ->description('Published at', position: 'above'),
+                        TextColumn::make('rater_id')
+                            ->label('Rater ID')
+                            ->numeric()
+                            ->tooltip('Filter by this rater')
+                            ->disabledClick()
+                            ->extraAttributes(function (Rating $record) {
+                                return [
+                                    'wire:click' => '$set("tableFilters.rater_id.value", "' . $record->rater_id . '", true)',
+                                    'class' => 'transition hover:text-primary-500 cursor-pointer',
+                                ];
+                            })
+                            ->description('Rater ID', position: 'above'),
+                        TextColumn::make('event_id')
+                            ->label('Rating ID')
+                            ->numeric()
+                            ->url(fn (Rating $record) => 'https://itch.io/event/' . $record->event_id)
+                            ->openUrlInNewTab()
+                            ->description('Rating ID', position: 'above'),
+                    ]),
+                    TextColumn::make('review')
+                        ->html()
+                        ->wrap()
+                        ->description('Review', position: 'above'),
+                ])
+                    ->from('xl')
+            ])
             ->filters([
                 SelectFilter::make('game')
                     ->relationship('game', 'name', fn (Builder $query) => $query->where('visible', true)),
